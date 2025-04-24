a108ad565591170e80907e8927db616160ae4aa6
valeriotheus <matheus.valerio6@etec.sp.gov.br>
2025-04-24 15:38:33 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(311, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Calculadora Básica";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(227, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Digite o 1° Numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(227, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(115, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Digite o 2° Numero";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(342, 116);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(342, 182);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(171, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(256, 223);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 67);
+            button1.TabIndex = 5;
+            button1.Text = "Somar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(377, 223);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 67);
+            button2.TabIndex = 6;
+            button2.Text = "Subtrair";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(377, 296);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 67);
+            button3.TabIndex = 7;
+            button3.Text = "Dividir";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(256, 296);
+            button4.Name = "button4";
+            button4.Size = new Size(115, 67);
+            button4.TabIndex = 8;
+            button4.Text = "Multiplicar";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(256, 401);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Resultado:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(324, 398);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(168, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(755, 454);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label4;
+        private TextBox textBox3;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,51 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void form1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1, num2, res;
+            num1 = Convert.ToDouble(textBox1.Text);  
+            num2 = Convert.ToDouble(textBox2.Text); 
+            res = num1 + num2;
+            textBox3.Text = res.ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num1, num2, res;
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            res = num1 - num2;
+            textBox3.Text = res.ToString();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double num1, num2, res;
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            res = num1 / num2;
+            textBox3.Text = res.ToString();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1, num2, res;
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            res = num1 * num2;
+            textBox3.Text = res.ToString();
+        }
+    }
+}
