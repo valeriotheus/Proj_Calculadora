bfab7547f3b13d5ed1e8beea4b2bf0849c94d10f
Matheus <matheus.valerio6@etec.sp.gov.br>
2025-04-25 20:49:13 -0300

    Ultimos ajustes

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -39,6 +39,7 @@
             button4 = new Button();
             label4 = new Label();
             textBox3 = new TextBox();
+            label5 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -47,9 +48,9 @@
             label1.BackColor = SystemColors.GradientInactiveCaption;
             label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.HotTrack;
-            label1.Location = new Point(311, 54);
+            label1.Location = new Point(355, 72);
             label1.Name = "label1";
-            label1.Size = new Size(138, 20);
+            label1.Size = new Size(176, 25);
             label1.TabIndex = 0;
             label1.Text = "Calculadora Básica";
             label1.Click += label1_Click;
@@ -59,44 +60,47 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.Highlight;
-            label2.Location = new Point(227, 119);
+            label2.Location = new Point(259, 159);
             label2.Name = "label2";
-            label2.Size = new Size(115, 15);
+            label2.Size = new Size(149, 20);
             label2.TabIndex = 1;
-            label2.Text = "Digite o 1° Numero";
+            label2.Text = "Digite o 1° Número:";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.Highlight;
-            label3.Location = new Point(227, 185);
+            label3.Location = new Point(259, 247);
             label3.Name = "label3";
-            label3.Size = new Size(115, 15);
+            label3.Size = new Size(149, 20);
             label3.TabIndex = 2;
-            label3.Text = "Digite o 2° Numero";
+            label3.Text = "Digite o 2° Número:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(342, 116);
+            textBox1.Location = new Point(410, 156);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(171, 23);
+            textBox1.Size = new Size(195, 27);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(342, 182);
+            textBox2.Location = new Point(410, 244);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(171, 23);
+            textBox2.Size = new Size(195, 27);
             textBox2.TabIndex = 4;
             // 
             // button1
             // 
             button1.BackColor = SystemColors.GradientActiveCaption;
             button1.ForeColor = SystemColors.HotTrack;
-            button1.Location = new Point(256, 223);
+            button1.Location = new Point(293, 297);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(115, 67);
+            button1.Size = new Size(131, 89);
             button1.TabIndex = 5;
             button1.Text = "Somar";
             button1.UseVisualStyleBackColor = false;
@@ -106,9 +110,10 @@
             // 
             button2.BackColor = SystemColors.GradientActiveCaption;
             button2.ForeColor = SystemColors.Highlight;
-            button2.Location = new Point(377, 223);
+            button2.Location = new Point(431, 297);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(115, 67);
+            button2.Size = new Size(131, 89);
             button2.TabIndex = 6;
             button2.Text = "Subtrair";
             button2.UseVisualStyleBackColor = false;
@@ -118,9 +123,10 @@
             // 
             button3.BackColor = SystemColors.GradientActiveCaption;
             button3.ForeColor = SystemColors.Highlight;
-            button3.Location = new Point(377, 296);
+            button3.Location = new Point(431, 395);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(115, 67);
+            button3.Size = new Size(131, 89);
             button3.TabIndex = 7;
             button3.Text = "Dividir";
             button3.UseVisualStyleBackColor = false;
@@ -130,9 +136,10 @@
             // 
             button4.BackColor = SystemColors.GradientActiveCaption;
             button4.ForeColor = SystemColors.Highlight;
-            button4.Location = new Point(256, 296);
+            button4.Location = new Point(293, 395);
+            button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Size = new Size(115, 67);
+            button4.Size = new Size(131, 89);
             button4.TabIndex = 8;
             button4.Text = "Multiplicar";
             button4.UseVisualStyleBackColor = false;
@@ -143,25 +150,38 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.HotTrack;
-            label4.Location = new Point(256, 401);
+            label4.Location = new Point(293, 535);
             label4.Name = "label4";
-            label4.Size = new Size(65, 15);
+            label4.Size = new Size(83, 20);
             label4.TabIndex = 9;
             label4.Text = "Resultado:";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(324, 398);
+            textBox3.Location = new Point(410, 532);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(168, 23);
+            textBox3.Size = new Size(191, 27);
             textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.HotTrack;
+            label5.Location = new Point(12, 579);
+            label5.Name = "label5";
+            label5.Size = new Size(241, 17);
+            label5.TabIndex = 11;
+            label5.Text = "Desenvolvido por: Matheus S. Valério";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
-            ClientSize = new Size(755, 454);
+            ClientSize = new Size(863, 605);
+            Controls.Add(label5);
             Controls.Add(textBox3);
             Controls.Add(label4);
             Controls.Add(button4);
@@ -173,6 +193,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -193,5 +214,6 @@
         private Button button4;
         private Label label4;
         private TextBox textBox3;
+        private Label label5;
     }
 }
